07ff0d6302a6fb65efd6b6afc373229320f40ae4
JAI SINGH <67551769+jaisrm98@users.noreply.github.com>
2020-07-11 18:41:11 +0530

    Add files via upload

---
+++ Char_to_string.java
@@ -0,0 +1,11 @@
+package coding;
+
+public class Char_to_string {
+
+	public static void main(String[] args) {
+		char ch='t';
+		String str=Character.toString(ch);
+System.out.println(str);
+	}
+
+}
