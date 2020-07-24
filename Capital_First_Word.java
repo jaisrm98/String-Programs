fab6a06fcd64da6041d80aa68174b4c9817dbc82
JAI SINGH <jai.srm98@gmail.com>
2020-07-24 19:44:57 +0530

    Add files via upload

---
+++ Capital_First_Word.java
@@ -0,0 +1,26 @@
+package coding;
+import java.util.*;
+public class Capital_First_Word {
+	public static void main(String[] args) {
+		String str;
+		String k="";
+		Scanner sc=new Scanner(System.in);
+		str=sc.nextLine();
+		String arr[]=str.split(" ");
+		
+		for(int i=0;i<arr.length;i++) {
+			arr[i]=arr[i].substring(0, 1).toUpperCase()+arr[i].substring(1);
+			k=k+arr[i];
+			k=k+" ";
+			
+		}
+		System.out.println(k.trim());
+		
+		
+		}
+			
+		}
+	 
+	
+
+
