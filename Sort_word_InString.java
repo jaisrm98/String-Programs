fc856e3b80b76e93e60131faaa15a821e09d5740
JAI SINGH <jai.srm98@gmail.com>
2020-08-12 17:50:57 +0530

    Add files via upload

---
+++ Sort_word_InString.java
@@ -0,0 +1,21 @@
+package coding;
+import java.util.*;
+public class Sort_word_InString {
+
+	public static void main(String[] args) {
+		String str;
+		
+		Scanner sc=new Scanner(System.in);
+		str=sc.nextLine();
+		
+		String arr[]=str.split(" ");
+		
+		
+		Arrays.sort(arr);;
+		for(int i=0;i<arr.length;i++)
+		{
+		System.out.print(arr[i]+" ");
+		}
+	}
+
+}
