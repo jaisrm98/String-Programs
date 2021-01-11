2a90d0f5eac565781743907bd603592eab6fd4b0
JAI SINGH <jai.srm98@gmail.com>
2021-01-11 15:44:57 +0530

    Update Check_character_in_string.java

--- Check_character_in_string.java
+++ Check_character_in_string.java
@@ -3,20 +3,27 @@ import java.util.*;
 public class Check_character_in_string {
 
 	public static void main(String[] args) {
-		boolean k=false;
-		char ch='m';
+		char ch='j';
 		String str="jai";
-		for(int i=0;i<str.length();i++)
-		{ if(str.charAt(i)==ch)
-			k=true;}
-			if(k==true)
-			System.out.println("true");
-		
+		int k=0;
+		char arr[]=str.toCharArray();
+		for(int i=0;i<arr.length;i++)
+		{
+			if(ch==arr[i])
+				k=1;
+			break;
+		}
+		if(k==1)
+		System.out.println("true");
 		else
-		
 			System.out.println("false");
+		
 		
 		
 	}
 
 }
+Input: j
+       jai
+     
+OUTPUT: true
