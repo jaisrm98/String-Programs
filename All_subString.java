ec67ee1d2f9de57e7f0684e55101cbd9baa62cdf
JAI SINGH <jai.srm98@gmail.com>
2021-01-11 15:09:49 +0530

    Update All_subString.java

--- All_subString.java
+++ All_subString.java
@@ -3,22 +3,22 @@ import java.util.*;
 public class All_subString {
 
 	public static void main(String[] args) {
-		String str="funksfundfkdfun";
+		String str="funksfundSfun";
 		String find="fun";
 		String s="";
 		int l=str.length();
 		ArrayList<String>list=new ArrayList<String>();
 		 for (int i = 0; i < l; i++)
 	      {
-	         for(int j = 1; j <= l - i; j++)
+	         for(int j = i+1; j <= l; j++)
 	         {
-	            s = str.substring(i, i+j);
+	            s = str.substring(i, j);
 	            if(s.equals(find))
 	            	list.add(s);
 	            
 	         }
 	      }
-		System.out.println(list.size());
+		System.out.println("total times "+list.size());
 	}
 
 }
