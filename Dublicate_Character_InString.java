c401b831692b20b80238639a7e3b79b5bda34190
JAI SINGH <jai.srm98@gmail.com>
2021-01-18 20:35:25 +0530

    Add files via upload

---
+++ Dublicate_Character_InString.java
@@ -0,0 +1,24 @@
+package coding;
+
+public class Dublicate_Character_InString {
+
+	public static void main(String[] args) {
+		String s="jai jas";
+		char arr[]=s.toCharArray();
+		int c=0;
+		for(int i=0;i<arr.length;i++)
+		{c=1;
+			for(int j=i+1;j<arr.length;j++)
+			{  
+				if(arr[i]==arr[j])
+				{
+					c++;
+					arr[j]='0';
+				}
+			}
+			if(arr[i]!='0' && c>1)
+				System.out.println(arr[i]);
+		}
+	}
+
+}
